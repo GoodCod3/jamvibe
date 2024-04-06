25aa4c690dc7501a9cfc0c22b86ab9261394ca18
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-04-06 23:57:47 +0200

    Configuring basic page structure

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,13 +9,22 @@ const RootLayout = ({ children }: IRootLayout) => {
     return (
         <>
             <div className="wrapper">
-                <header>Header</header>
+                <header>
+                    Jam Vibe
+                </header>
                 <article>
-                    <h1>Welcome</h1>
-                    <p>Hi!</p>
+                    {children}
                 </article>
-                <aside><ul><li>Sidebar</li></ul></aside>
-                <footer>Footer</footer>
+                <aside>
+                    <ul>
+                        <li>Home</li>
+                    </ul>
+                </aside>
+                <footer>
+                    Made with 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+                    by Jojo
+                </footer>
             </div>
         </>
     );
