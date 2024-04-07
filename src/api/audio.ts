7a96bc653e6455b4ef201717441489cd1b4d5314
Jonathan Rodríguez Alejos <jrodriguez.5716@gmail.com>
2024-04-07 15:10:28 +0200

    Recording audio

---
+++ src/api/audio.ts
@@ -0,0 +1,57 @@
+class AudioAPI {
+    private mediaRecorder: MediaRecorder | null = null;
+    private recordedChunks: Blob[] = [];
+
+    constructor() {
+        if ((window && (window as any).AudioContext) && navigator.mediaDevices.getUserMedia) {
+            this.startRecording = this.startRecording.bind(this);
+        } else {
+            throw new Error('Your browser is not compatible, you have to update it.');
+        }
+    }
+
+    isBrowserCompatible() {
+        return !!((window && (window as any).AudioContext) && navigator.mediaDevices.getUserMedia);
+
+    }
+
+    async startRecording() {
+        if (this.isBrowserCompatible()) {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+                this.mediaRecorder = new MediaRecorder(stream);
+                this.mediaRecorder.ondataavailable = this.handleDataAvailable.bind(this);
+                this.mediaRecorder.start();
+            } catch (error) {
+                console.error('Error accessing microphone:', error);
+            }
+        }
+    }
+
+    handleDataAvailable(event: BlobEvent) {
+        if (event.data.size > 0) {
+            this.recordedChunks.push(event.data);
+        }
+    }
+
+    stopRecording() {
+        if (this.mediaRecorder && this.mediaRecorder.state === 'recording') {
+            this.mediaRecorder.stop();
+
+            return this.recordedChunks;
+        }
+    }
+
+    getAudiosRecorded() {
+        return this.recordedChunks;
+    }
+
+    playRecordedAudio() {
+        const blob = new Blob(this.recordedChunks, { type: 'audio/wav' });
+        const audioUrl = URL.createObjectURL(blob);
+        const recordedAudio = document.getElementById('recordedAudio') as HTMLAudioElement;
+        recordedAudio.src = audioUrl;
+    }
+}
+
+export default AudioAPI;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,22 @@ import React, {
     useState,
     useMemo,
     useCallback,
+    useEffect,
 } from 'react';
 import Timeline from 'wavesurfer.js/dist/plugins/timeline.esm.js'
 
 import RootLayout from '@/components/Layout';
 import { useWavesurfer } from '@wavesurfer/react';
+import AudioAPI from '@/api/audio';
 
 
 const formatTime = (seconds: number) => [seconds / 60, seconds % 60].map((v) => `0${Math.floor(v)}`.slice(-2)).join(':')
 
-
 const Home = () => {
+    const [isRecording, setIsRecording] = useState(false);
+    const [audiosRecorded, setAudiosRecorded] = useState<Blob[]>([]);
     const containerRef = useRef(null);
+    const audioAPIRef = useRef(new AudioAPI());
     const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
         container: containerRef,
         waveColor: 'rgb(200, 0, 200)',
@@ -30,50 +34,81 @@ const Home = () => {
     });
 
     const onPlayPause = useCallback(() => {
-        wavesurfer && wavesurfer.playPause()
+        wavesurfer && wavesurfer.playPause();
     }, [wavesurfer]);
 
     const onClickNewRecord = () => {
+        if (isRecording) {
+            const audioBlobs = audioAPIRef.current.stopRecording();
+            console.log(audioBlobs);
+            if (audioBlobs) {
+                setAudiosRecorded([...audioBlobs]);
+            }
+            setIsRecording(false);
 
+        } else {
+            audioAPIRef.current.startRecording();
+            setIsRecording(true);
+        }
     };
 
-    return (
-        <RootLayout>
-            <div className="save_audio">
-                <div className="audio_data">
-                    <div className="file_name">
-                        <input type='text' />
+    if (audioAPIRef.current.isBrowserCompatible()) {
+
+        return (
+            <RootLayout>
+                <div className="save_audio">
+                    <div className="audio_data">
+                        <div className="file_name">
+                            <input type='text' />
+                        </div>
+                        <div className="current_time">
+                            <p>Current time: {formatTime(currentTime)}</p>
+                        </div>
+                        <div className="export_audio">
+                            <button style={{ minWidth: '5em' }}>
+                                Save file
+                            </button>
+                        </div>
                     </div>
-                    <div className="current_time">
-                        <p>Current time: {formatTime(currentTime)}</p>
-                    </div>
-                    <div className="export_audio">
-                        <button style={{ minWidth: '5em' }}>
-                            Save file
+                    <div className="play_audio">
+                        <button onClick={onPlayPause} style={{ minWidth: '5em' }}>
+                            {isPlaying ? 'Pause' : 'Play'}
                         </button>
                     </div>
                 </div>
-                <div className="play_audio">
-                    <button onClick={onPlayPause} style={{ minWidth: '5em' }}>
-                        {isPlaying ? 'Pause' : 'Play'}
-                    </button>
-                </div>
-            </div>
-            <div ref={containerRef} />
+                <div ref={containerRef} />
 
 
-            <div className="content">
-                <div className="effects"></div>
-                <div className="list_audio">
-                    <div className="record_button">
-                        <button style={{ minWidth: '5em' }}>
-                            New Record
-                        </button>
+                <div className="content">
+                    <div className="effects"></div>
+                    <div className="list_audio">
+                        <div className="record_button">
+                            <button onClick={onClickNewRecord}>
+                                {isRecording ? 'Stop Recording' : 'Start Recording'}
+                            </button>
+                        </div>
+
+                        <div className="record_audios">
+                            <ul>
+                                {audiosRecorded.map((audioRecorded, index) => (
+                                    <li key={`recorded_${index}`}>Record #{index * 1} - {audioRecorded.size}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </RootLayout>
-    );
+            </RootLayout>
+        );
+    } else {
+        return (
+            <RootLayout>
+                <div className="compability_error">
+                    <h1>Your browser is not compatible, please update it.</h1>
+                </div>
+            </RootLayout>
+
+        )
+    }
 };
 
 export default Home;
